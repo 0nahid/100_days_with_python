5bb0e5e3ebfcab371da6b14933f487b6daa5e6f3
0nahid <nahidhassanbulbul@gmail.com>
2023-03-04 01:06:47 +0600

    added => data_types, conversion & checking

---
+++ data_types.py
@@ -0,0 +1,28 @@
+# string
+print("Hello"[1])
+print("Hello"[4])
+
+# integer
+print(123 + 345)
+print(123_456_789)
+
+# float
+print(3.14159)
+
+# boolean
+print(True)
+print(False)
+
+# Type Error
+# print("Hello" + 5)
+# print("123" + 5)
+
+# Type Conversion
+print("123" + str(5))
+print(int("123") + 5)
+
+# Type Checking
+print(type("Hello"))
+print(type(123))
+print(type(3.14159))
+print(type(True))
