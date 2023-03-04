2d3b1d8d73bf1a09fad018746b12b026c5424a2e
0nahid <nahidhassanbulbul@gmail.com>
2023-03-04 23:57:39 +0600

    added => maths.py

---
+++ maths.py
@@ -0,0 +1,18 @@
+# PEMDAS
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print("The sum is: " + str(x+y)) # add
+
+
+print("The difference is: " + str(x-y)) # subtract
+
+
+print("The product is: " + str(x*y)) # multiply
